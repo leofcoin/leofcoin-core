a309251ebf5f5b2d6e1a34bca9720d70ad88eeca
VandeurenGlenn <vandeurenglenn@gmail.com>
2018-04-09 01:07:23 +0200

    :meat_on_bone: Cleanup & update dependencies

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,5 @@
 import { initConnection } from './lib/network/secure-client-connection';
-import { getUserConfig, debug, allowFailureUntillEnough } from './utils';
+import { getUserConfig, debug, allowFailureUntillEnough, log } from './utils';
 import bus from './lib/bus';
 import { join } from 'path';
 import './server/index';
@@ -34,14 +34,14 @@ export const core = async ({ genesis, network }) => {
       }, 10);
       //graceful shutdown
     });
-    // await connect([connection.address]);
-    // const connection_now = Date.now();
+    await connect();
+    const connection_now = Date.now();
     bus.emit('stage-two');
     console.groupCollapsed('prepare for init');
     // console.log(`secure-connection took: ${(secure_now - now) / 1000} seconds`);
-    console.log(`ipfs daemon startup took: ${(ipfsd_now - now) / 1000} seconds`);
-    // console.log(`peer connection took: ${(connection_now - ipfsd_now) / 1000} seconds`);
-    console.log(`total load prep took ${(Date.now() - now) / 1000} seconds`);
+    log(`ipfs daemon startup took: ${(ipfsd_now - now) / 1000} seconds`);
+    log(`peer connection took: ${(connection_now - ipfsd_now) / 1000} seconds`);
+    log(`total load prep took ${(Date.now() - now) / 1000} seconds`);
     console.groupEnd();
     await write(configPath, JSON.stringify(config, null, '\t'));
 
