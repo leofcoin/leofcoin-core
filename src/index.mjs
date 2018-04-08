e617027001ddf313af5548ccb5af9143ee89b146
Glenn Vandeuren <vandeurenglenn@gmail.com>
2018-04-09 01:11:16 +0200

    Unbootstrap (#8)
    * :metal: Remove crypto-daemon
    
    * :cactus: Cleanup dependencies & logs
    
    * :metal: unbootstrap

--- src/index.mjs
+++ src/index.mjs
@@ -20,8 +20,8 @@ export const core = async ({ genesis, network }) => {
 	try {
     const now = Date.now();
     const config = await getUserConfig;
-    const connection = await initConnection;
-    const secure_now = Date.now();
+    // const connection = await initConnection;
+    // const secure_now = Date.now();
     bus.emit('stage-one');
     const {ipfsd, repo} = await IPFSNode();
 
@@ -34,13 +34,13 @@ export const core = async ({ genesis, network }) => {
       }, 10);
       //graceful shutdown
     });
-    await connect([connection.address]);
-    const connection_now = Date.now();
+    // await connect([connection.address]);
+    // const connection_now = Date.now();
     bus.emit('stage-two');
     console.groupCollapsed('prepare for init');
-    console.log(`secure-connection took: ${(secure_now - now) / 1000} seconds`);
-    console.log(`ipfs daemon startup took: ${(ipfsd_now - secure_now) / 1000} seconds`);
-    console.log(`peer connection took: ${(connection_now - ipfsd_now) / 1000} seconds`);
+    // console.log(`secure-connection took: ${(secure_now - now) / 1000} seconds`);
+    console.log(`ipfs daemon startup took: ${(ipfsd_now - now) / 1000} seconds`);
+    // console.log(`peer connection took: ${(connection_now - ipfsd_now) / 1000} seconds`);
     console.log(`total load prep took ${(Date.now() - now) / 1000} seconds`);
     console.groupEnd();
     await write(configPath, JSON.stringify(config, null, '\t'));
