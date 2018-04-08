eda00b73b3b491df81d19c6422748596c345baf4
VandeurenGlenn <vandeurenglenn@gmail.com>
2018-04-09 01:06:45 +0200

    :metal: Update genesis creation

--- src/params.js
+++ src/params.js
@@ -1,5 +1,7 @@
 import { join } from 'path';
 import { homedir } from 'os';
+import { decode } from 'bs58';
+import { read } from 'crypto-io-fs';
 // paths
 export let olivia;
 export let genesis;
@@ -22,6 +24,7 @@ export const netKeyPath = join(networkPath, 'swarm.key');
 
 export const localCurrent = join(networkPath, 'db', 'current');
 export const localIndex = join(networkPath, 'db', 'index');
+export const localDAGAddress = join(networkPath, 'db', 'dag.multiaddress');
 
 // export const
 // TODO: remove seed once we have a static ip for our ipfs daemon node
@@ -34,7 +37,16 @@ export const configPath = join(AppData, 'core.config');
 export const reward = 150;
 export const consensusSubsidyInterval = 52500;
 export const consensusSubsidyPercentage = 12.5; // quarterlings
-export const genesisCID = '1220000014cee068f2102c760660e43456beaa101161e98be0f7b7281296c7afdf0f'; // // TODO: should be hardcoded
+export const genesisCID = '12200000248bfb7fea0ba4ff67b3ff7370c0683cc8cdab540a47766e147fa3b58566'; // // TODO: should be hardcoded
+
+export const GENESISBS58 = 'CtoFKgJCQ46vL4WpCykNvRTu2RohfYe35c9ULjz2stMi3u47PmYmPX8LYBLA9r1QKufjkVW43EtRZ4pd7wRwhHNt7RmZMeN8hqU7XDJ4';
+export const GENESISBLOCK = (() => {
+	const block = JSON.parse(Buffer.from(decode(GENESISBS58)).toString());
+	block.hash = genesisCID.substring(4);
+	return block;
+})();
+export const localDAGMultiaddress = (async () => await read(localDAGAddress, 'string'))()
+
 export const checkpoints = [
 ];
 
