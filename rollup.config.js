bdbdd0c5a9a0958051e9ba912db471900a6cd96b
VandeurenGlenn <vandeurenglenn@gmail.com>
2018-04-18 00:10:08 +0200

    :construction: Add intro, banner & footer

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+const readFileSync = require('fs').readFileSync;
+const npmPackage = readFileSync('package.json', 'utf8');
+const { version, name } = JSON.parse(npmPackage);
 export default [
 	// ES module version, for modern browsers
 	{
@@ -5,31 +8,25 @@ export default [
 		output: {
 			dir: 'dist/module',
 			format: 'es',
-			sourcemap: true
+			sourcemap: true,
+			intro: `const ENVIRONMENT = {version: '${version}', production: true};`,
+			banner: `/* ${name} version ${version} */`,
+			footer: '/* follow Leofcoin on Twitter! @leofcoin */'
 		},
 		experimentalCodeSplitting: true,
 		experimentalDynamicImport: true
 	},
 
-	// SystemJS version, for older browsers
-	{
-		input: ['src/core.js', 'src/lib/workers/miner-worker.js'],
-		output: {
-			dir: 'dist/nomodule',
-			format: 'system',
-			sourcemap: true
-		},
-		experimentalCodeSplitting: true,
-		experimentalDynamicImport: true
-	},
-
-	// SystemJS version, for older browsers
+	// CommonJS version, for Node, Browserify & Webpack
 	{
 		input: ['src/core.js', 'src/lib/workers/miner-worker.js'],
 		output: {
 			dir: 'dist/commonjs',
 			format: 'cjs',
-			sourcemap: true
+			sourcemap: true,
+			intro: `const ENVIRONMENT = {version: '${version}', production: true};`,
+			banner: `/* ${name} version ${version} */`,
+			footer: '/* follow Leofcoin on Twitter! @leofcoin */'
 		},
 		experimentalCodeSplitting: true,
 		experimentalDynamicImport: true
