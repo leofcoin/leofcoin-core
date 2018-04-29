848e797adfb1290bf350cd002ccd1f7e04ae378b
VandeurenGlenn <vandeurenglenn@gmail.com>
2018-04-29 02:51:50 +0200

    :metal: Add netPrefix

--- src/lib/network/ipfs-star.js
+++ src/lib/network/ipfs-star.js
@@ -1,4 +1,5 @@
 import { encode } from 'bs58';
+import { netPrefix } from './../../params'
 // TODO: rename to client
 global.peerset = new Map();
 /**
@@ -19,26 +20,26 @@ const peerconnect = message => {
  * @param {method} options.subscribe pubsub subscriber
  * @param {method} options.publish pubsub publisher
  */
-export default (address, network, pubsub) => {
+export default (address, pubsub) => {
 	if (!pubsub && !global.ipfs) throw Error('pubsub client not found');
 	else if (!pubsub && global.ipfs) pubsub = global.ipfs.pubsub;
-  if (!network) throw Error('network required');
 	const {subscribe, publish, unsubscribe} = pubsub;
-  const prefix = network === 'leofcoin' ? 'leofcoin-' : 'leofcoin-olivia-';
   const peers = message => {
-    subscribe(encode(Buffer.from(`${prefix}peernet-peer-connect`)), peerconnect);
+    subscribe(encode(Buffer.from(`${netPrefix}peernet-peer-connect`)), peerconnect);
     JSON.parse(message.data.toString()).forEach(peer => {
       peerset.set(peer[0], peer[1]);
     })
-  	unsubscribe(encode(Buffer.from(`${prefix}peernet-peers`)), () => {});
+  	unsubscribe(encode(Buffer.from(`${netPrefix}peernet-peers`)), () => {});
   };
-	subscribe(encode(Buffer.from(`${prefix}peernet-peers`)), peers);
-  subscribe(encode(Buffer.from(`${prefix}peernet-peer-disconnect`)), peerdisconnect);
+	subscribe(encode(Buffer.from(`${netPrefix}peernet-peers`)), peers);
+  subscribe(encode(Buffer.from(`${netPrefix}peernet-peer-disconnect`)), peerdisconnect);
   // let the network know there is a new peer
-	publish(encode(Buffer.from(`${prefix}peernet`)), new Buffer(address));
+	setTimeout(function () {
+    publish(encode(Buffer.from(`${netPrefix}peernet`)), new Buffer(address));
+  }, 500);
 	return {
 		stop: async () => {
-	    publish(encode(Buffer.from(`${prefix}peernet-peer-disconnect`)), Buffer(address));
+	    publish(encode(Buffer.from(`${netPrefix}peernet-peer-disconnect`)), Buffer(address));
 		},
     peers: () => peerset
 	};
--- src/params.js
+++ src/params.js
@@ -1,23 +1,66 @@
 import { join } from 'path';
 import { homedir } from 'os';
-import { decode } from 'bs58';
+import { decode, encode } from 'bs58';
 import { read } from 'crypto-io-fs';
+import { keccak } from 'leofcoin-hash';
+const argv = process.argv;
 
 export const networks = {
 	'leofcoin': join(homedir(), '.leofcoin'),
 	'olivia': join(homedir(), '.leofcoin/olivia')
 };
 
-export const signalServers = [
-  '/ip4/84.193.79.27/tcp/4002/ipfs/QmXN5cqP2rd38f2zsDjAM2Y4geXF3jegEma392fdSohfx3'
-]
+export const network = (() => {
+  const index = argv.indexOf('--network');
+  return process.env.NETWORK || (index > -1) ? argv[index + 1] : 'leofcoin';
+})()
+
+export const verbose = Boolean([
+  argv.indexOf('-v'),
+  argv.indexOf('--verbose'),
+  process.env.VERBOSE ? 1 : -1
+].reduce((p, c) => {
+  if (c > p) return c;
+  return Number(p)
+}, -1) >= 0);
 
 export const olivia = process.argv.includes('olivia') || process.argv.includes('testnet');
 export const genesis = process.argv.includes('genesis');
-export const network = olivia ? 'olivia' : 'leofcoin';
 export const AppData = join(homedir(), 'AppData', 'Roaming', 'Leofcoin');
+const netHash = net => encode(keccak(Buffer.from(`${net}-`), 256)).slice(0, 24);
 
+export const mainNethash = netHash('leofcoin');
 
+/**
+ * returns the hash for a subnet, prefixed with mainNethash
+ */
+const subnetHash = net => {
+  const prefix = mainNethash.slice(0, 4);
+  const hash = netHash(net);
+  return `${prefix}${hash.slice(4, hash.length)}`
+}
+export const testNethash = subnetHash('olivia');
+
+export const netPrefix = (() => network === 'leofcoin' ? mainNethash : testNethash)()
+export const signalServers = (() => {
+  if (network === 'olivia') return [
+    '/ip4/162.208.10.171/tcp/4002/ipfs/QmNwuenPwwVKuQRHksR4YHtWS91n5PoyqyK3Focbm1uU4d'
+  ]
+  else return [
+    '/ip4/162.208.10.171/tcp/4002/ipfs/QmXWTPiAg52FH87p7nVMcJVmMUzmLVLRDpT1yh1apb9xKr'
+  ]
+})()
+
+export const netKeys = {
+  olivia: `/key/swarm/psk/1.0.0/
+  /base16/
+  b37e0b6f3574931ce7a0ef863f64b0f01ba111bb7fabb6a661fc67b51b4ddd15`,
+  leofcoin: `/key/swarm/psk/1.0.0/
+  /base16/
+  0b78a0dcb430dd77311ab6629aa6b75fa05c6779a567dcc176b2299853e6f746`
+}
+
+export const netKey = netKeys[network];
 export const networkPath = networks[network];
 export const netKeyPath = join(networkPath, 'swarm.key');
 export const localCurrent = join(networkPath, 'db', 'current');
