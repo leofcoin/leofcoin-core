34206729fd9f1725b456dbca3c96b1a1fd15230d
Glenn Vandeuren <vandeurenglenn@gmail.com>
2018-10-03 20:18:38 +0200

    :metal: [peernet] add resolveAddresses

--- src/lib/network/peernet.js
+++ src/lib/network/peernet.js
@@ -11,9 +11,6 @@ import ipfs from './ipfs-mock';
 const handleDefaultBootstrapAddresses = async addresses => {
   try {
     let bootstrap = await ipfs.config.get('Bootstrap');
-    // const peers = await ipfs.swarm.peers();
-    // peers.forEach(peer => console.log(peer.peer.toB58String()))
-    // addresses.forEach(address => !bootstrap.indexOf(address) )
     for (const peer of addresses) {
       for (const address of bootstrap) {
         if (addresses.indexOf(address) === -1) {
@@ -38,6 +35,7 @@ const handleDefaultBootstrapAddresses = async addresses => {
 
 let done = false;
 let runs = 0;
+
 export const resolvePeers = () => new Promise(resolve => {
   if (runs === 5) {
     debug('searched for peers but none found, returning empty array');
@@ -53,6 +51,19 @@ export const resolvePeers = () => new Promise(resolve => {
   runs++;
   if (!done) setTimeout(() => resolvePeers().then(peers => resolves(peers)), 1000);
 })
+
+export const resolveAddresses = async () => {
+  const peers = await resolvePeers();
+  return peers.map(({addr, peer}) => `${addr.toString()}/ipfs/${peer.toB58String()}`);
+}
+
+/**
+ * Get peers from 'star' peerset or resolve from network
+ */
+const getPeerAddresses = async peers => {
+  if (global.peerset.size === 0) return resolveAddresses();
+  return [...global.peerset.values()]
+}
 
 export const _connect = async addresses =>
   new Promise(async (resolve, reject) => {
@@ -74,26 +85,18 @@ export const connectBootstrap = async addresses => {
 
   succes(`connected to ${signalServers.length} bootstrap peer(s)`);
 }
-export const connect = (addresses = []) => new Promise(async (resolve, reject) => {
+export const connect = (peers = []) => new Promise(async (resolve, reject) => {
   try {
     bus.emit('connecting', true);
     debug('connecting peers');
-    // await handleDefaultBootstrapAddresses(addresses);
+
     const { id } = await ipfs.id();
     // TODO: filter using peerrep
-    const peers = await resolvePeers();
-    // transform peers into valid ipfs addresses
-    const addrs = peers.map(({addr, peer}) => `${addr.toString()}/ipfs/${peer.toB58String()}`);
-    // if (peers && peers.length === 0) peers = addresses;
+    peers = await getPeerAddresses();
+    await _connect(peers);
 
-    await _connect(addrs);
-    
-    succes(`connected to ${addrs.length} peer(s)`);
+    succes(`connected to ${peers.length} peer(s)`);
 
-    ipfs.pubsub.subscribe('peer-connected', event => {
-      // TODO: update reputations
-      if (event.from !== id) console.log(event); // announcepeer
-    });
     ipfs.pubsub.publish('peer-connected', new Buffer(id));
     bus.emit('connecting', false);
     resolve();
