661b6d54ec1459132713dfc8381f45d2551346b5
vandeurenglenn <vandeurenglenn@gmail.com>
2018-10-05 00:39:40 +0200

    :metal: cleanup startup process

--- bin/cli.js
+++ bin/cli.js
@@ -2,8 +2,7 @@
 
 const params = {
 	genesis: false,
-	dynamic: false,
-	network: 'main'
+	dynamic: false
 };
 
 for (const param of process.argv) {
@@ -12,14 +11,8 @@ for (const param of process.argv) {
 	case 'genesis':
 		params.genesis = true;
 		break;
-	case 'olivia':
-		params.network = 'olivia';
-		break;
-		// wether or not to seed ip address to our DynamicSeedDB
-	case 'dynamic-seed':
-		params.dynamic = true;
-		break;
 	}
+
 }
 
 const { core } = require('./../dist/commonjs/core.js');
